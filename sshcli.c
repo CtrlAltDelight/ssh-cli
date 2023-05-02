b9bb774a25d33cd5c0e234606525ecf9453f066d
CtrlAltDelight <lbchigges@gmail.com>
2023-05-02 10:40:35 -0400

    Changed to argp. Still needs to be tested.

--- sshcli.c
+++ sshcli.c
@@ -4,10 +4,82 @@
 #include <stdbool.h>
 #include <string.h>
 #include <assert.h>
-#include <getopt.h>
+#include <argp.h>
+
+// TODO: finish implementing argp
+// Adapted from https://www.gnu.org/software/libc/manual/html_node/Argp-Example-3.html
+const char *argp_program_version = "sshcli 1.1";
+const char *argp_program_bug_address = "<git+git@ctrlaltdelight.org>";
+static char doc[] = "sshcli -- Connects to your favorite ssh connections quickly.";
+
+static struct argp_option options[] = {
+	{"host_idx", 'n', "value", 0, "Select which line to use from the file."},
+	{ 0 }
+};
+
+static char args_doc[] = "";
+struct arguments {
+	//char *args[2];                /* arg1 & arg2 */
+	int host_idx;
+};
+
+static error_t parse_opt (int key, char *arg, struct argp_state *state) {
+	/* Get the input argument from argp_parse, which we
+	   know is a pointer to our arguments structure. */
+	struct arguments *arguments = state->input;
+
+	switch (key) {
+		case 'n':
+			arguments->host_idx = atoi(arg);
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+
+	/* Keeping a copy while working locally
+	switch (key) {
+		case 'n':
+			arguments->host_idx = 1;
+			break;
+		case 'v':
+			arguments->verbose = 1;
+			break;
+		case 'o':
+			arguments->output_file = arg;
+			break;
+
+		case ARGP_KEY_ARG:
+			if (state->arg_num >= 2)
+			// Too many arguments.
+			argp_usage (state);
+
+			arguments->args[state->arg_num] = arg;
+
+			break;
+
+		case ARGP_KEY_END:
+			if (state->arg_num < 2)
+			// Not enough arguments.
+			argp_usage (state);
+			break;
+
+		default:
+			return ARGP_ERR_UNKNOWN;
+    }
+	*/
+
+	return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
+// implementing argp end
+
+
 
 #define MAX_LINE_SIZE 300
 
+/*
 static void _print_help() {
 	printf("usage: sshcli [-h] [-n [HOST_NUM]]\n");
 	printf("Note: if not HOST_NUM specified, HOST_NUM is 0\n");
@@ -17,49 +89,19 @@ static void _print_help() {
 	printf("-h, --help     show this help message and exit\n");
 	printf("\n");
 }
+*/
 
 int main(int argc, char* argv[]) {
+	struct arguments arguments;
 
-	int host_idx = -1; // should change when option is given TODO: make this into a required argument or find better method of getting the hostname.
+	// Default values for arguments.
+	arguments.host_idx = -1;
+	
+	// Parse the arguments.
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
 	// Get options
 	// TODO: Add a way to edit hostnames_file on the command line.
-	int c;
-	while(1) {
-		int option_index = 0;
-		static struct option long_opts[] = {
-			{ .name = "host_num", .has_arg = required_argument, .flag = NULL, .val = 'n' },
-			{ .name = "help", .has_arg = no_argument, .flag = NULL, .val = 'h' }
-		};
-
-		c = getopt_long(argc, argv, "hn:", long_opts, &option_index);
-		//printf("c == %d\n", c);
-
-		if(c == -1) {
-			break;
-		}
-		switch(c) {
-			case 0:
-				printf("long option %s", long_opts[option_index].name);
-				if(optarg) {
-					printf(" with arg %s", optarg);
-				}
-				printf("\n");
-				break;
-			case 1:
-				printf("regular argument '%s'\n", optarg);
-				break;
-			case 'n':
-				printf("option n with value '%s'\n", optarg ? optarg : NULL);
-				host_idx = atoi(optarg); // The +1 skips past the = sign
-				break;
-			case 'h':
-				_print_help();
-				return EXIT_SUCCESS;
-			default:
-				printf("?? getopt returnded character code 0%o ??\n", c);
-		}
-	}
 
 	// get hostnames.txt
 	char hostnames_filepath[200];
@@ -80,7 +122,7 @@ int main(int argc, char* argv[]) {
 
 	// get hostname
 	char hostname[MAX_LINE_SIZE];
-	for(int i = 0; fgets(hostname, sizeof hostname, hostnames_file) != NULL && i < host_idx; i++);
+	for(int i = 0; fgets(hostname, sizeof hostname, hostnames_file) != NULL && i < arguments.host_idx; i++);
 	char ssh_command[MAX_LINE_SIZE];
 	strcpy(ssh_command, "ssh ");
 	strcat(ssh_command, hostname);
